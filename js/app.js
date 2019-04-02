462ea3aba605b81548ed1a4466d5635fbea2c936
chidrome <chidrome@gmail.com>
2019-04-01 22:33:14 -0700

    functions to calculate random number of customers per hour and cookiesSoldPerHour have been created

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ var pike = {
   avgCookiePerSale: 6.3,
   customersPerHour: [],
   cookiesPerHour: [],
-  total: 0
+  totalCookiesSoldForDay: 0
 };
 
 var seatac = {
@@ -17,7 +17,7 @@ var seatac = {
   avgCookiePerSale: 1.2,
   customersPerHour: [],
   cookiesPerHour: [],
-  total: 0
+  totalCookiesSoldForDay: 0
 };
 
 var seattleCenter = {
@@ -27,7 +27,7 @@ var seattleCenter = {
   avgCookiePerSale: 3.7,
   customersPerHour: [],
   cookiesPerHour: [],
-  total: 0
+  totalCookiesSoldForDay: 0
 };
 
 var capitolHill = {
@@ -37,7 +37,7 @@ var capitolHill = {
   avgCookiePerSale: 2.3,
   customersPerHour: [],
   cookiesPerHour: [],
-  total: 0
+  totalCookiesSoldForDay: 0
 };
 
 var alki = {
@@ -47,5 +47,41 @@ var alki = {
   avgCookiePerSale: 4.6,
   customersPerHour: [],
   cookiesPerHour: [],
-  total: 0
+  totalCookiesSoldForDay: 0
 };
+
+var storeHours = [
+  '6am',
+  '7am',
+  '8am',
+  '9am',
+  '10am',
+  '11am',
+  '12pm',
+  '1pm',
+  '2pm',
+  '3pm',
+  '4pm',
+  '5pm',
+  '7pm',
+  '8pm',
+];
+
+////////////////// HELPER FUNCTIONS //////////////////
+
+numberOfCustomersPerHour = (min, max) => {
+  let customersPerHour = []
+  for(let i = 0; i < storeHours.length; i++){
+    customersPerHour.push(Math.floor(Math.random() * (max - min) + min));
+  }
+  return customersPerHour;
+};
+
+cookiesSoldPerHour = (arrayOfCustomersPerHour, avgCookiePerSale) =>{
+  let cookiesPerHour = [];
+  for(let i = 0; i < arrayOfCustomersPerHour.length; i++){
+    cookiesPerHour.push(Math.round(arrayOfCustomersPerHour[i] * avgCookiePerSale * 10) / 10);
+  }
+  return cookiesPerHour;
+};
+
