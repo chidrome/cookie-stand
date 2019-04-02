e42c3b7dc011738ac74ccd62b099aa86f55b3358
chidrome <chidrome@gmail.com>
2019-04-01 21:58:13 -0700

    store objects created

---
+++ js/app.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var pike = {
+  location: '1st and Pike',
+  minCustomer: 23,
+  maxCustomer: 65,
+  avgCookiePerSale: 6.3,
+  customersPerHour: [],
+  cookiesPerHour: [],
+  total: 0
+};
+
+var seatac = {
+  location: 'Seatac Airport',
+  minCustomer: 3,
+  maxCustomer: 24,
+  avgCookiePerSale: 1.2,
+  customersPerHour: [],
+  cookiesPerHour: [],
+  total: 0
+};
+
+var seattleCenter = {
+  location: 'Seattle Center',
+  minCustomer: 11,
+  maxCustomer: 38,
+  avgCookiePerSale: 3.7,
+  customersPerHour: [],
+  cookiesPerHour: [],
+  total: 0
+};
+
+var capitolHill = {
+  location: 'Capitol Hill',
+  minCustomer: 20,
+  maxCustomer: 38,
+  avgCookiePerSale: 2.3,
+  customersPerHour: [],
+  cookiesPerHour: [],
+  total: 0
+};
+
+var alki = {
+  location: 'Alki',
+  minCustomer: 2,
+  maxCustomer: 16,
+  avgCookiePerSale: 4.6,
+  customersPerHour: [],
+  cookiesPerHour: [],
+  total: 0
+};
